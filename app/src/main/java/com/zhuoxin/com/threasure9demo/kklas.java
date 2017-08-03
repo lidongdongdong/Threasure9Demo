0f988ef619c8b0102f520741d0feb797290def22
“lidongdongdong <1842563749@qq.com>
2017-08-03 15:53:51 +0800

    主界面drawLayout

---
+++ app/src/main/java/com/zhuoxin/com/threasure9demo/kklas.java
@@ -0,0 +1,12 @@
+package com.zhuoxin.com.threasure9demo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+
+public class kklas extends Fragment{
+
+
+}
